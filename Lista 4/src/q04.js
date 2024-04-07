737e22f46ae20cda1d63efac5d75eb1710648bc6
Fernanda <gilneidefernandaf@gmail.com>
2024-04-07 19:46:43 -0300

    Q04 com conjuntos

--- Lista 4/src/q04.js
+++ Lista 4/src/q04.js
@@ -1,5 +1,3 @@
-import Conjunto from "../structures/Conjunto.js";
-
 export default function verificarVencedor(numerosSorteados, apostas){
     const vencedoresSena = []
     const vencedoresQuina = []
@@ -8,7 +6,7 @@ export default function verificarVencedor(numerosSorteados, apostas){
     for (let aposta of apostas) {  
         let acertos = []
         i++;
-        
+
         for (let numero of aposta) {
             if (numerosSorteados.has(numero)) {
                 acertos.push(numero);
@@ -24,8 +22,38 @@ export default function verificarVencedor(numerosSorteados, apostas){
     }
 
     //console.log("\nVencedor(es) da Sena:", vencedoresSena);
-    console.log("\nVencedor(es) da Quina:", vencedoresQuina);
+    //console.log("\nVencedor(es) da Quina:", vencedoresQuina);
 
     return vencedoresSena.length > 0 || vencedoresQuina.length > 0;
 }
 
+//Código alternativo:
+
+// import Conjunto from "../structures/Conjunto.js";
+// export default function verificarVencedor(numerosSorteados, apostas){
+
+//     const vencedoresSena = new Conjunto();
+//     const vencedoresQuina = new Conjunto();
+   
+//     let count = 0;
+//     let ind_sena, ind_quina;
+//     for (let aposta of apostas) {
+//         const acertos = new Conjunto();
+
+//         for (let numero of aposta) {
+//             if (numerosSorteados.has(numero)) {
+//                 acertos.add(numero);
+//             }
+//         }
+
+//         if (acertos.size() === 6) {
+//             ind_sena = count;
+//             vencedoresSena.add(aposta);
+//         } else if (acertos.size() === 5) {
+//             ind_quina = count;
+//             vencedoresQuina.add(aposta);
+//         }
+//         count++;
+//     }
+//     return vencedoresSena.size() > 0 || vencedoresQuina.size() > 0;
+// }
